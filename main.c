1987316755a5da915de2122bb29b2cbb19b0097c
Syed Akmal <sayasyedakmal@gmail.com>
2024-02-28 23:10:07 +0800

    practicing variable and getting user input

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 /*
-    Constant
+    Getting User Input
 
-    - cant be modified
-    - common practice - the const variable is written in UPPERCASE
+    - try get user input and practice variable and datatype
+        - name - string
+        - age - int
+        - height - double
+        - weight - float
+        - grade - character
+    - print it back
 
 */
 
@@ -11,9 +16,48 @@
 
 int main()
 {
-    const int NUM = 5;
-    printf("%d\n",NUM);
-    NUM = 8; //will throw error
-    printf("%d",NUM);
+    char fullName[20];
+    char grade;
+    int age;
+    double height;
+    float weight;
+
+    printf("Enter your full name: ");
+    fgets(fullName,20,stdin);
+
+    printf("Enter your grade: ");
+    scanf("%c", &grade);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Enter your height(cm): ");
+    scanf("%lf", &height);
+
+    printf("Enter your weight(kg): ");
+    scanf("%f", &weight);
+
+    // Remove the newline character, if it exists
+    size_t len = strlen(fullName);
+    if (len > 0 && fullName[len - 1] == '\n') {
+        fullName[len - 1] = '\0';
+    }
+
+    printf("\n\n");
+    printf("-----Your Information------");
+    printf("\n");
+    printf("Name: %s", fullName);
+    printf("\n");
+    printf("Age: %d", age);
+    printf("\n");
+    printf("Height: %f", height);
+    printf("\n");
+    printf("Weight: %f", weight);
+    printf("\n");
+    printf("Grade: %c", grade);
+    printf("\n");
+
+
+
     return 0;
 }
