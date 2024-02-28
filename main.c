39f2169cfc603b93f4c80c162f8ea486f98648d6
Syed Akmal <sayasyedakmal@gmail.com>
2024-02-28 21:24:53 +0800

    Learning c variable

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 
 int main()
 {
-    printf("Hello, Syed Akmal\n");
+    char characterName[] = "Syed Akmal";
+    int characterAge = 36;
+
+    printf("There once was a man named %s\n", characterName);
+    printf("he was %d years old.\n", characterAge);
+    printf("He really liked the name %s\n", characterName);
+    printf("but did not like being %d.\n", characterAge);
     return 0;
 }
